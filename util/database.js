8960497878655ef9af88355429126f09a423e3ac
sang.nguyen <sang.nguyen@enablestartup.com>
2021-11-28 20:11:05 +0700

    Update product

--- util/database.js
+++ util/database.js
@@ -1,7 +1,30 @@
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize("node-complete", "root", "Sangbk_@S208", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-module.exports = sequelize;
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://sangbk208:sangbk208@devconnector.rzudl.mongodb.net/shop?retryWrites=true&w=majority",
+    { useUnifiedTopology: true }
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
